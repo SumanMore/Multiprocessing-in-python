09718b17077fe74977fd754c9b5e66ae665bcb8b
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-06-02 00:12:00 +0530

    Update p1 Introduction.py

--- p1 Introduction.py
+++ p1 Introduction.py
@@ -1,3 +1,26 @@
+
+The multiprocessing package supports spawning processes. It refers to a function that loads and executes a new child processes. 
+For the child to terminate or to continue executing concurrent computing,then the current process hasto wait using an API, 
+which is similar to threading module.
+When we work with Multiprocessing,at first we create process object. Then it calls a start() method.
+
+Example-1:
+   from multiprocessing import Process
+   def display():
+      print ('Hi !! I am Python')
+      if __name__ == '__main__':
+      p = Process(target=display)
+      p.start()
+      p.join()
+In this example, at first we import the Process class then initiate Process object with the display() function.
+Then process is started with start() method and then complete the process with the join() method.
+
+
+
+Example 2:
+We can also pass arguments to the function using args keyword.
+In this example, we create two process that calculates the cube and squares of numbers and prints all results to the console.
+
 import time
 import multiprocessing
 
@@ -21,3 +44,7 @@ if __name__ == "__main__":
     p2.join()
 
     print("Done!")
+We can also create more than one process at atime.
+In this example, at first we create one process which is process1, this process just calculates the square of a number and at the same
+time second process process2 is calculates the cube of a number.
+
