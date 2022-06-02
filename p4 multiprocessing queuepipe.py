1e5388dc54c7a96fa7f8f11c17ff0e96a0ff6e75
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-06-02 11:16:27 +0530

    Update p4 multiprocessing queuepipe.py

--- p4 multiprocessing queuepipe.py
+++ p4 multiprocessing queuepipe.py
@@ -1,12 +1,15 @@
-When we pass data between processes then at that time we can use Queue object.
+#sharing data between processes using queue
+
+
+#When we pass data between processes then at that time we can use Queue object.
 
 import multiprocessing
 
-def calc_square(numbers, q):
+def calc_square(numbers, q): #child process
     for n in numbers:
-        q.put(n*n)
+        q.put(n*n) #inserting data at the end of the queue
 
-if __name__ == "__main__":
+if __name__ == "__main__": #parent process
     numbers = [2,3,5]
     q = multiprocessing.Queue()
     p = multiprocessing.Process(target=calc_square, args=(numbers,q))
@@ -15,7 +18,7 @@ if __name__ == "__main__":
     p.join()
 
     while q.empty() is False:
-        print(q.get())
+        print(q.get()) #iterating through queue and printing the elements
         
         
 In this example, at first create a function that cal square of a number and inserting in the queue
